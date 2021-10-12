6c2611ab43b9ea3a779b94851645a951d885a1d4
uzaaft <muaf@nmbu.no>
2021-10-12 10:37:46 +0200

    Removed unused dep.

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@ extern crate diesel;
 extern crate serde_derive;
 
 use actix_web::{web, web::Data, App, HttpServer};
-use dotenv;
 
 use crate::models::Measurement;
 pub mod models;
@@ -13,21 +12,18 @@ use crate::models::Pool;
 use std::env;
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    std::env::set_var("RUST_LOG", "actix_web=debug");
     let port = env::var("PORT").unwrap_or_else(|_| "3000".to_string());
-
     let api_route = format!("0.0.0.0:{}", port);
-    dotenv::dotenv();
     let db_url = match std::env::var("DATABASE_URL") {
         Ok(db_url) => db_url,
         Err(_) => {
-            println!("DATABASE_URL not set, creating empty container");
+            println!("DATABASE_URL not set");
             panic!()
         }
     };
     let pool = crate::models::init_pool(&db_url);
 
-    std::env::set_var("RUST_LOG", "actix_web=debug");
-    dotenv::dotenv().ok();
     // Start http server
     HttpServer::new(move || {
         App::new()
